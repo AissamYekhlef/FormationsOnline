60fb6c95a6f9129d950e33e104be891dd028c65d
issam <issamyayou@gmail.com>
2020-08-15 00:07:26 +0100

    added profile link to the user.

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -23,7 +23,7 @@ class StudentController extends Controller
      */
     public function index()
     {
-        return view('student.classroom');
+        return view('user.classroom');
     }
 
 }
